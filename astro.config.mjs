17da44aca85693ed37256b81a824037649168388
Jamedie <mail@jimmygaucher.com>
2025-08-29 14:53:56 +0200

    update sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,20 @@
+// astro.config.mjs
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://jimmygaucher.com",
-  integrations: [sitemap()],
+  integrations: [
+    sitemap({
+      i18n: {
+        defaultLocale: "fr",
+        locales: {
+          fr: "fr",
+          en: "en",
+        },
+      },
+      exclude: ["/404", "/fr/404", "/en/404"],
+    }),
+  ],
+  trailingSlash: "never",
 });
